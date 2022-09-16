ccd73258265ebf4ac9390953d97d652287a53b6e
milkdue <69843097+milkdue@users.noreply.github.com>
2022-09-16 20:22:26 +0800

    Delete api/utils directory

--- api/utils/getIp.ts
+++
@@ -1,9 +0,0 @@
-/*
- * @Author: 可以清心
- * @Description: 
- * @Date: 2022-09-16 19:57:46
- * @LastEditTime: 2022-09-16 19:58:48
- */
-module.exports = function(req){
-    return req.headers["x-real-ip"];
-}
--- api/utils/getLocation.ts
+++
@@ -1,6 +0,0 @@
-/*
- * @Author: 可以清心
- * @Description: 
- * @Date: 2022-09-16 19:48:30
- * @LastEditTime: 2022-09-16 19:48:41
- */
