98a6d03a58545679a78696370d5c6d6179bed99c
milkdue <321432896@qq.com>
2024-02-19 20:30:28 +0800

    feat(error): 新增error

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@
  * @Author: 可以清心
  * @Description: 
  * @Date: 2024-02-19 18:56:21
- * @LastEditTime: 2024-02-19 18:59:39
+ * @LastEditTime: 2024-02-19 19:08:22
  */
 const getIp = require("./apis/getIp");
 const getLocation = require("./apis/getLocation");
@@ -10,7 +10,7 @@ const getVerificationCode = require("./apis/getVerificationCode");
 const getError = require("./apis/getError")
 
 module.exports = (req: any, res: any) => {  
-    switch(req.body.method){
+    switch(req.body.method) {
         case 'getIp':
             getIp(req, res);
             break;
