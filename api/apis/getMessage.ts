8072c389493eab9d4cc5f440d2c55bc5aa71284d
milkdue <321432896@qq.com>
2024-02-27 14:29:30 +0800

    feat(message): 新增message接口

---
+++ api/apis/getMessage.ts
@@ -0,0 +1,12 @@
+/*
+ * @Author: 可以清心
+ * @Description: 
+ * @Date: 2024-02-19 18:58:11
+ * @LastEditTime: 2024-02-27 14:27:22
+ */
+module.exports = function(res){
+    res.status(200).json({
+        code: "ok",
+        message: "已存在THSQ202402200004退货申请单退货的品种,已存在THSQ202402200004退货申请单退货的品种,请确认是否还需要提交退货申请?"
+    });
+}
--- api/index.ts
+++ api/index.ts
@@ -2,12 +2,13 @@
  * @Author: 可以清心
  * @Description: 
  * @Date: 2024-02-19 18:56:21
- * @LastEditTime: 2024-02-19 19:08:22
+ * @LastEditTime: 2024-02-27 14:28:58
  */
 const getIp = require("./apis/getIp");
 const getLocation = require("./apis/getLocation");
 const getVerificationCode = require("./apis/getVerificationCode");
-const getError = require("./apis/getError")
+const getError = require("./apis/getError");
+const getMessage = require("./apis/getMessage");
 
 module.exports = (req: any, res: any) => {  
     switch(req.body.method) {
@@ -22,6 +23,9 @@ module.exports = (req: any, res: any) => {
             break;
         case "error":
             getError(res);
+            break;
+        case "message":
+            getMessage(res);
             break;
     }
 }
